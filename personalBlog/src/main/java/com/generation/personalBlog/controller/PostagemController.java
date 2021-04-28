d284756573de7b1a02f50821d7055c25e2fcf63b
kiota-rita <rita-kiota@hotmail.com>
2021-04-28 14:41:33 -0300

    blogCrud

--- personalBlog/src/main/java/com/generation/personalBlog/controller/PostagemController.java
+++ personalBlog/src/main/java/com/generation/personalBlog/controller/PostagemController.java
@@ -3,17 +3,22 @@ package com.generation.personalBlog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.personalBlog.model.Postagem;
 import com.generation.personalBlog.repository.PostagemRepository;
 
-import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
+
 
 @RestController //informa que esta classse é um controlador
 @RequestMapping("/postagens")
@@ -38,4 +43,18 @@ public class PostagemController {
 			return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 		}
 	
+	@PostMapping //permite alimentar os campos do db
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){ //pega o que vem no corpo da requisição
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem)); //passa a confirmação de que a postagem foi criada e salva
+	}
+	
+	@PutMapping //atualiza os campos do db
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem){ //pega o que vem no corpo da requisição
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem)); //salva a postagem e diz que o status da requisição foi ok
+	}
+	
+	@DeleteMapping("/{id}") //deleta a postagem pelo numero do id
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
